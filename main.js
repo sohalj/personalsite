2574fee58285536d1a78d518568c55bfc822f2e9
sohalj <sohalj@students.wwu.edu>
2018-03-12 17:52:32 -0700

    made updates with window sizing

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
+
+function setAttribute(elem, attr){
+  Object.keys(attr).forEach(key => elem.setAttribute(key, attr[key]));
+}
+
+
+
+
+function loadImages(picLinks){
+  //var size = picLinks.length(); --only have two pic right now
+  console.log(picLinks);
+  var link = null;
+ for(i=0; i<6; i++){
+    link = picLinks[i];   
+    var div = document.createElement("div");
+    setAttribute(div, {"class": "col-sm-6"});
+    var a = document.createElement("a");
+    setAttribute(a, {href: link, target: "_blank"})
+    var img = document.createElement("img");
+    setAttribute(img, {class: "img-responsive", src: link, alt: "WWU Dashboards"});
+    a.appendChild(img);
+    div.appendChild(a)
+    var pic = document.getElementById("images")
+    pic.appendChild(div);
+  }
+}
+
+
 $(document).ready(function(){
+  var picLinks = ["http://i.imgur.com/UZqEIOB.png", "http://cdn.wallpapersafari.com/40/14/Gutken.jpg", "http://cdn.wallpapersafari.com/40/14/Gutken.jpg",
+                  "http://cdn.wallpapersafari.com/40/14/Gutken.jpg","http://cdn.wallpapersafari.com/40/14/Gutken.jpg","http://cdn.wallpapersafari.com/40/14/Gutken.jpg",
+                  ];
+  loadImages(picLinks);
+
+
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
